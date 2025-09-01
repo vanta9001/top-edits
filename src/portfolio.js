ca4f418327df97b4ffa7d8ce3fb854ffdac8cd14
thefriendlyhacker <bzogo7@gmail.com>
2025-09-01 20:40:36 +0000

    edit social links

--- src/portfolio.js
+++ src/portfolio.js
@@ -11,9 +11,9 @@ const seo = {
   description:
     "We are a video editing company dedicated to turning raw footage into professional, engaging, and polished content. Whether it’s for businesses, creators, or personal projects, we focus on making videos that connect with audiences and tell stories in a clear and creative way. Our goal is simple: you film it, we make it shine.",
   og: {
-    title: "Ashutosh Hathidara Portfolio",
+    title: "Top Edits",
     type: "website",
-    url: "http://ashutoshhathidara.com/",
+    url: "https://top-edits.vercel.app/",
   },
 };
 
@@ -23,66 +23,51 @@ const greeting = {
   logo_name: "AshutoshHathidara",
   nickname: "layman_brother",
   subTitle:
-    "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
+    "We are a video editing company dedicated to turning raw footage into professional, engaging, and polished content. Whether it’s for businesses, creators, or personal projects, we focus on making videos that connect with audiences and tell stories in a clear and creative way. Our goal is simple: you film it, we make it shine.",
   resumeLink:
-    "https://drive.google.com/file/d/1bXRknv_h-XI_3CQ3SGPteGODtvEb7YvI/view?usp=sharing",
-  portfolio_repository: "https://github.com/ashutosh1919/masterPortfolio",
-  githubProfile: "https://github.com/ashutosh1919",
+    "https://github.com/Axion-Industries",
+  portfolio_repository: "https://github.com/Axion-Industries",
+  githubProfile: "https://github.com/Axion-Industries",
 };
 
-const socialMediaLinks = [
-  /* Your Social Media Link */
-  // github: "https://github.com/ashutosh1919",
-  // linkedin: "https://www.linkedin.com/in/ashutosh-hathidara-88710b138/",
-  // gmail: "ashutoshhathidara98@gmail.com",
-  // gitlab: "https://gitlab.com/ashutoshhathidara98",
-  // facebook: "https://www.facebook.com/laymanbrother.19/",
-  // twitter: "https://twitter.com/ashutosh_1919",
-  // instagram: "https://www.instagram.com/layman_brother/"
+// Simple key-value links
+const socialMediaLinks = {
+  github: "https://github.com/Axion-Industries",
+  threads: "https://www.threads.net/@bogdanzogovic123",
+  gmail: "axionindustries.official@gmail.com",
+  gitlab: "https://gitlab.com/Axion-Industries/",
+  instagram: "https://www.instagram.com/@bogdanzogovic123"
+};
 
+// Detailed social media config list
+const socialMediaDetails = [
   {
     name: "Github",
-    link: "https://github.com/ashutosh1919",
-    fontAwesomeIcon: "fa-github", // Reference https://fontawesome.com/icons/github?style=brands
-    backgroundColor: "#181717", // Reference https://simpleicons.org/?q=github
+    link: "https://github.com/Axion-Industries",
+    fontAwesomeIcon: "fa-github",
+    backgroundColor: "#181717",
   },
   {
-    name: "LinkedIn",
-    link: "https://www.linkedin.com/in/ashutosh1919/",
-    fontAwesomeIcon: "fa-linkedin-in", // Reference https://fontawesome.com/icons/linkedin-in?style=brands
-    backgroundColor: "#0077B5", // Reference https://simpleicons.org/?q=linkedin
-  },
-  {
-    name: "YouTube",
-    link: "https://youtube.com/c/DevSense19",
-    fontAwesomeIcon: "fa-youtube", // Reference https://fontawesome.com/icons/youtube?style=brands
-    backgroundColor: "#FF0000", // Reference https://simpleicons.org/?q=youtube
+    name: "Threads",
+    link: "https://www.threads.net/@bogdanzogovic123",
+    customIcon: "/src/assets/images/threads.png", // your file
+    backgroundColor: "#000000",
   },
   {
     name: "Gmail",
-    link: "mailto:ashutoshhathidara98@gmail.com",
-    fontAwesomeIcon: "fa-google", // Reference https://fontawesome.com/icons/google?style=brands
-    backgroundColor: "#D14836", // Reference https://simpleicons.org/?q=gmail
-  },
-  {
-    name: "X-Twitter",
-    link: "https://twitter.com/ashutosh_1919",
-    fontAwesomeIcon: "fa-x-twitter", // Reference https://fontawesome.com/icons/x-twitter?f=brands&s=solid
-    backgroundColor: "#000000", // Reference https://simpleicons.org/?q=x
-  },
-  {
-    name: "Facebook",
-    link: "https://www.facebook.com/laymanbrother.19/",
-    fontAwesomeIcon: "fa-facebook-f", // Reference https://fontawesome.com/icons/facebook-f?style=brands
-    backgroundColor: "#1877F2", // Reference https://simpleicons.org/?q=facebook
+    link: "mailto:axionindustries.official@gmail.com",
+    fontAwesomeIcon: "fa-google",
+    backgroundColor: "#D14836",
   },
   {
     name: "Instagram",
-    link: "https://www.instagram.com/layman_brother/",
-    fontAwesomeIcon: "fa-instagram", // Reference https://fontawesome.com/icons/instagram?style=brands
-    backgroundColor: "#E4405F", // Reference https://simpleicons.org/?q=instagram
+    link: "https://www.instagram.com/@bogdanzogovic123/",
+    fontAwesomeIcon: "fa-instagram",
+    backgroundColor: "#E4405F",
   },
 ];
+
+
 
 const skills = {
   data: [
